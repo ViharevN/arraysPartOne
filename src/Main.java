92b8f611beffb31375ee8e476f742c70ca92ea65
ViharevN <swix_off@mail.ru>
2022-10-17 23:08:04 +0300

    домашнее задание массивы часть 1. задание 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Массивы часть 1
-        //задание 1
+        //задание 1.1
 
         int [] arr = new int[3];
         arr[0] = 1;
@@ -10,6 +10,17 @@ public class Main {
         System.out.println(arr[0]);
         System.out.println(arr[1]);
         System.out.println(arr[2]);
+
+        //задание 1.2
+
+        double [] arrTwo = {1.57, 7.654, 9.986};
+        System.out.println(arrTwo[0]);
+        System.out.println(arrTwo[1]);
+        System.out.println(arrTwo[2]);
+
+
+
+
 
 
     }
